9b986eefc781a3d4950aa1a59fb320fc286879e4
YassirID <yassir.id345@gmail.com>
2025-06-28 00:14:44 +0700

    add back start toggle, fix hamburger

--- src/script.js
+++ src/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
 };    
 
@@ -19,4 +24,12 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+// Close menu when clicking outside
+document.addEventListener('click', function(event) {
+    if (event.target !== hamburger && navMenu){  
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
 });
